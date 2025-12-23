816e5b8f78f48be8a4b6834b597ab2bfeb879eea
FeltSense <feltsense62@gmail.com>
2025-12-23 18:57:57 -0300

    🔧 Build Fix: Updated app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import Navigation from '@/components/Navigation'
 import Hero from '@/components/Hero'
-
 import HeroKeyFeaturesHighlight from '@/components/HeroKeyFeaturesHighlight'
 import CoreProductFeatures from '@/components/CoreProductFeatures'
 import Benefits from '@/components/Benefits'
@@ -19,28 +18,19 @@ export default function Home() {
   return (
     <main className="overflow-hidden bg-white">
       <Navigation />
-      
       <Hero />
-
-      
       <HeroKeyFeaturesHighlight />
-      
       <CoreProductFeatures />
-      
       <Benefits />
-      
       <div id="premium-features">
         <PremiumFeaturesSection />
       </div>
-      
       <Testimonials />
       <CustomerSuccessStories />
-      
       <div id="beta-signup">
         <BetaSignup />
       </div>
       <Pricing />
-      
       <FAQ />
       <ContactForm />
       <Footer />
